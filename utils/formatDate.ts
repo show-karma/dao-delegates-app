336a1e37113813410487cd8cd375f9d1fec59234
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2025-02-13 19:20:30 -0300

    feat: delegate oracle scores

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,6 +1,10 @@
 import moment from 'moment';
 
-type IFormat = 'MMM YYYY' | 'MMMM D, YYYY' | 'MMM D, YYYY';
+type IFormat =
+  | 'MMM YYYY'
+  | 'MMMM D, YYYY'
+  | 'MMM D, YYYY'
+  | 'MMM D, YYYY HH:mm';
 
 export const formatDate = (
   dateToFormat: string,
